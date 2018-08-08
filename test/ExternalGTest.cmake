eeb408e06670c125affd7be71cd9251c12e30b4c
Vance Miller <vance@cs.utexas.edu>
2018-08-08 14:07:29 -0500

    fix googletest release tag

--- test/ExternalGTest.cmake
+++ test/ExternalGTest.cmake
@@ -5,6 +5,7 @@ include(ExternalProject)
 ExternalProject_Add(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG release-1.8.0
   UPDATE_COMMAND ""
   INSTALL_COMMAND ""
   LOG_DOWNLOAD ON
